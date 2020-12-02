2f50106e097d96eb5c7eb93cdf0fd83dca4cd933
Olha Danylchenko <danylchenkoolha@gmail.com>
2020-12-02 16:12:17 +0100

    end of 1st advent

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // 1st
 // find the two entries that sum to 2020 and then multiply those two numbers together
 
-const count = (arr) => {
+const twoNums = (arr) => {
   let sum = 0;
   let secondNum = 0;
 
@@ -14,4 +14,23 @@ const count = (arr) => {
     }
   }
   console.log(sum);
+};
+
+// 2nd
+// find the three entries that sum to 2020 and then multiply those two numbers together
+
+const threeNums = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      let thirdNum = 2020 - arr[i] - arr[j];
+
+      if (
+        arr.indexOf(thirdNum) !== -1 &&
+        arr.indexOf(thirdNum) !== arr[i] &&
+        arr.indexOf(thirdNum) !== arr[j]
+      ) {
+        return arr[i] * arr[j] * thirdNum;
+      }
+    }
+  }
 };
